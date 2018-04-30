32b299d54e7ea4f206b5743a741540ccdc78ddc6
Lime-blur <cc-lime@yandex.ru>
2018-04-30 17:18:38 +0400

    Uploaded all program files.

---
+++ Unit1.pas
@@ -0,0 +1,198 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, jpeg, ExtCtrls, StdCtrls, Mask, XPMan, Grids, ValEdit, ComCtrls;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Shape1: TShape;
+    Shape2: TShape;
+    Label1: TLabel;
+    Edit1: TEdit;
+    Label2: TLabel;
+    Label3: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    Edit2: TEdit;
+    XPManifest1: TXPManifest;
+    Button3: TButton;
+    Button4: TButton;
+    ListBox1: TListBox;
+    Label4: TLabel;
+    Button5: TButton;
+    Label5: TLabel;
+    Shape3: TShape;
+    Timer1: TTimer;
+    procedure Button4Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses Unit2;
+
+{$R *.dfm}
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+ Close;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  fs: TFileStream;
+  s: AnsiString;
+begin
+  if (Edit1.Text <> '') and (Edit2.Text <> '') then begin
+    Button1.Enabled := false;
+    Button2.Enabled := false;
+    Button3.Enabled := true;
+    Button5.Enabled := true;
+    if not FileExists('C:\CCchecker\Users.dat') then begin
+      fs := TFileStream.Create('C:\CCchecker\Users.dat', fmCreate);
+      try
+        s := '';
+        fs.WriteBuffer(s[1], Length(s));
+      finally
+        fs.Free;
+      end;
+    end;
+    ListBox1.Items.LoadFromFile('C:\CCchecker\Users.dat');
+    ListBox1.Items.Add(Edit1.Text);
+    ListBox1.Items.SaveToFile('C:\CCchecker\Users.dat');
+    Label5.Caption := 'Вы вошли как: '+Edit1.Text;
+    Edit1.Enabled := false;
+    Edit2.Enabled := false;
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  fs: TFileStream;
+  s: AnsiString;
+begin
+  if (Edit1.Text <> '') and (Edit2.Text <> '') then begin
+    Button1.Enabled := false;
+    Button2.Enabled := false;
+    Button3.Enabled := true;
+    Button5.Enabled := true;
+    if not FileExists('C:\CCchecker\Users.dat') then begin
+      fs := TFileStream.Create('C:\CCchecker\Users.dat', fmCreate);
+      try
+        s := '';
+        fs.WriteBuffer(s[1], Length(s));
+      finally
+        fs.Free;
+      end;
+    end;
+    ListBox1.Items.LoadFromFile('C:\CCchecker\Users.dat');
+    ListBox1.Items.Add(Edit1.Text);
+    ListBox1.Items.SaveToFile('C:\CCchecker\Users.dat');
+    Label5.Caption := 'Вы вошли как: '+Edit1.Text;
+    Edit1.Enabled := false;
+    Edit2.Enabled := false;
+  end;
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+var
+  List: TStringList;
+  i: integer;
+begin
+    Button1.Enabled := true;
+    Button2.Enabled := true;
+    Button3.Enabled := false;
+    Button5.Enabled := false;
+    Label5.Caption := 'Вы не вошли в аккаунт';
+    try
+      List := TStringList.Create;
+      List.LoadFromFile('C:\CCchecker\Users.dat');
+    except
+      MessageDlg('Ошибка файла!', mtError, [mbOk], 0);
+    end;
+    for i := List.Count-1 downto 0 do begin
+      if Pos(Edit1.Text, List.Strings[i]) <> 0 then List.Delete(i);
+    end;
+    List.SaveToFile('C:\CCchecker\Users.dat');
+    List.Free;
+    Edit1.Text := '';
+    Edit2.Text := '';
+    Edit1.Enabled := true;
+    Edit2.Enabled := true;
+    ListBox1.Clear;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  fs: TFileStream;
+  s: AnsiString;
+begin
+  if (ListBox1.ItemIndex >= 0) then begin
+    if (ListBox1.Items[ListBox1.ItemIndex] <> Edit1.Text) then begin
+      Form2.Caption := 'Чат - '+ListBox1.Items[ListBox1.ItemIndex];
+      Form2.Show;
+      Timer1.Enabled := false;
+      if not FileExists('C:\CCchecker\'+ListBox1.Items[ListBox1.ItemIndex]+'_sended.dat') then begin
+        fs := TFileStream.Create('C:\CCchecker\'+ListBox1.Items[ListBox1.ItemIndex]+'_sended.dat', fmCreate);
+        try
+          s := '';
+          fs.WriteBuffer(s[1], Length(s));
+        finally
+          fs.Free;
+        end;
+      end;
+    end
+    else
+      MessageDlg('Вы не можете общаться с самим собой!', mtWarning, [mbOk], 0);
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  if not DirectoryExists('C:\CChecker') then
+    CreateDirectory('C:\CCchecker', 0);
+    Timer1.Enabled := true;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+var
+  List: TStringList;
+  i: integer;
+begin
+  try
+    List := TStringList.Create;
+    List.LoadFromFile('C:\CCchecker\Users.dat');
+  except
+    MessageDlg('Ошибка файла!', mtError, [mbOk], 0);
+  end;
+  for i := List.Count-1 downto 0 do begin
+    if Pos(Edit1.Text, List.Strings[i]) <> 0 then List.Delete(i);
+  end;
+  List.SaveToFile('C:\CCchecker\Users.dat');
+  List.Free;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  if Edit2.Enabled = false then
+  ListBox1.Items.LoadFromFile('C:\CCchecker\Users.dat');
+end;
+
+end.
---
+++ Unit2.pas
@@ -0,0 +1,138 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, StdCtrls, SynEdit, SynMemo, XPMan, Menus,
+  SynEditHighlighter, SynHighlighterCpp;
+
+type
+  TForm2 = class(TForm)
+    XPManifest1: TXPManifest;
+    SynCppSyn1: TSynCppSyn;
+    MainMenu1: TMainMenu;
+    N1: TMenuItem;
+    Edit1: TEdit;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Splitter2: TSplitter;
+    SynMemo1: TSynMemo;
+    SynMemo2: TSynMemo;
+    Memo1: TMemo;
+    Splitter1: TSplitter;
+    Button1: TButton;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    Timer1: TTimer;
+    SaveDialog1: TSaveDialog;
+    procedure Button1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure N2Click(Sender: TObject);
+    procedure N4Click(Sender: TObject);
+    procedure N5Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+  userName, opponentName: string;
+
+implementation
+
+uses Unit1;
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+  fs: TFileStream;
+  s: AnsiString;
+begin
+  if not FileExists('C:\CCchecker\chat_.dat') then begin
+    fs := TFileStream.Create('C:\CCchecker\chat_.dat', fmCreate);
+    try
+      s := '';
+      fs.WriteBuffer(s[1], Length(s));
+    finally
+      fs.Free;
+    end;
+  end
+  else
+    Memo1.Lines.LoadFromFile('C:\CCchecker\chat_.dat');
+  Memo1.Lines.Add(userName+': '+Edit1.Text);
+  Memo1.Lines.SaveToFile('C:\CCchecker\chat_.dat');
+  Edit1.Clear;
+end;
+
+procedure TForm2.FormShow(Sender: TObject);
+begin
+  userName := Form1.Edit1.Text;
+  opponentName := Form1.ListBox1.Items[Form1.ListBox1.ItemIndex];
+  SynMemo1.Clear;
+  SynMemo2.Clear;
+  Memo1.Clear;
+  Timer1.Enabled := true;
+end;
+
+procedure TForm2.N2Click(Sender: TObject);
+begin
+ if OpenDialog1.Execute then
+ SynMemo1.Lines.LoadFromFile(OpenDialog1.FileName);
+end;
+
+procedure TForm2.N4Click(Sender: TObject);
+begin
+  if SaveDialog1.Execute then
+    SynMemo1.Lines.SaveToFile(SaveDialog1.FileName);
+end;
+
+procedure TForm2.N5Click(Sender: TObject);
+begin
+  if SaveDialog1.Execute then
+    SynMemo2.Lines.SaveToFile(SaveDialog1.FileName);
+end;
+
+procedure TForm2.Timer1Timer(Sender: TObject);
+var
+  fs: TFileStream;
+  s: AnsiString;
+begin
+  if not FileExists('C:\CCchecker\'+userName+'_sended.dat') then begin
+    fs := TFileStream.Create('C:\CCchecker\'+userName+'_sended.dat', fmCreate);
+    try
+      s := '';
+      fs.WriteBuffer(s[1], Length(s));
+    finally
+      fs.Free;
+    end;
+  end;
+  if not FileExists('C:\CCchecker\'+opponentName+'_sended.dat') then begin
+    fs := TFileStream.Create('C:\CCchecker\'+opponentName+'_sended.dat', fmCreate);
+    try
+      s := '';
+      fs.WriteBuffer(s[1], Length(s));
+    finally
+      fs.Free;
+    end;
+  end;
+  if FileExists('C:\CCchecker\chat_.dat') then
+    Memo1.Lines.LoadFromFile('C:\CCchecker\chat_.dat');
+  SynMemo1.Lines.SaveToFile('C:\CCchecker\'+userName+'_sended.dat');
+  SynMemo2.Lines.LoadFromFile('C:\CCchecker\'+opponentName+'_sended.dat');
+end;
+
+procedure TForm2.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+    Timer1.Enabled := false;
+end;
+
+end.
